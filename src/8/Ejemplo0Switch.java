cc95a09a37f0a9acecf289d64918385ca5b98a60
zubiri <zubiri@zubiri-VirtualBox.(none)>
2014-10-16 19:44:28 +0200

    e

--- src/8/Ejemplo0Switch.java
+++ src/8/Ejemplo0Switch.java
@@ -10,31 +10,31 @@ public class Ejemplo0Switch {  // clase principal del programa.
         mes = sc.nextInt();//guardamos el valor introducido en pantalla en mes.
         switch (mes) // estructuramos para comparar una lista multiple posibilidades solo se utiliza con int o char.
         {
-                case 1: System.out.println("ENERO"); //si la condicion case es igual al switch muestra el system.out.printl en pantalla
+              case 1: System.out.println("ENERO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
                            break; //corta la ejecucion y no evalua las siguientes.
-                case 2: System.out.println("FEBRERO");
-                           break;
-                case 3: System.out.println("MARZO");
-                           break;
-                case 4: System.out.println("ABRIL");
-                           break;
-                case 5: System.out.println("MAYO");
-                           break;
-                case 6: System.out.println("JUNIO");
-                           break;
-                case 7: System.out.println("JULIO");
-                           break;
-                case 8: System.out.println("AGOSTO");
-                           break;
-                case 9: System.out.println("SEPTIEMBRE");
-                           break;
-                case 10: System.out.println("OCTUBRE");
-                             break;
-                case 11: System.out.println("NOVIEMBRE");
-                             break;
-                case 12: System.out.println("DICIEMBRE");
-                             break;
-                default : System.out.println("Mes no válido"); // si la variable switch no coincide con el mes entoces entra al defaul                     
+              case 2: System.out.println("FEBRERO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+              case 3: System.out.println("MARZO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+              case 4: System.out.println("ABRIL");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+              case 5: System.out.println("MAYO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+              case 6: System.out.println("JUNIO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+              case 7: System.out.println("JULIO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+              case 8: System.out.println("AGOSTO");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+           case 9: System.out.println("SEPTIEMBRE");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                           break; //corta la ejecucion y no evalua las siguientes.
+             case 10: System.out.println("OCTUBRE");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                             break; //corta la ejecucion y no evalua las siguientes.
+           case 11: System.out.println("NOVIEMBRE");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                             break; //corta la ejecucion y no evalua las siguientes.
+           case 12: System.out.println("DICIEMBRE");//si la condicion case es igual al switch muestra el system.out.printl en pantalla
+                             break; //corta la ejecucion y no evalua las siguientes.
+              default : System.out.println("Mes no válido"); //si la variable switch no coincide con case  entoces entra al defaul                     
         }
     }
 }
--- src/9/Ejemplo1Switch.java
+++ src/9/Ejemplo1Switch.java
@@ -1,40 +1,40 @@
 /*
  * Programa que pide dos números y un operador y muestra el resultado
  */
-import java.util.*;
-import java.io.*;
-public class Ejemplo1Switch {
-    public static void main(String[] args) throws IOException{
-        int A,B, Resultado = 0 ;
-        char operador;
-        boolean calculado = true;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca un numero entero:");
-        A = sc.nextInt();
-        System.out.print("Introduzca otro numero entero:");
-        B = sc.nextInt();
-        System.out.print("Introduzca un operador (+,-,*,/):");
-        operador = (char)System.in.read();
-        switch (operador) {
-                case '-' : Resultado = A - B;
-                           break;
-                case '+' : Resultado = A + B;
-                           break;
-                case '*' : Resultado = A * B;
-                           break;
-                case '/' : if(B!=0)
-                              Resultado = A / B;
-                           else{
-                              System.out.println("\nNo se puede dividir por cero");
-                              calculado = false;
+import java.util.*; //importa las clases que permiten acceso al sistema.
+import java.io.*; //importa las clases que nos permite gestionar las entradas y salidas del sistema.
+public class Ejemplo1Switch { // clase principal del programa.
+    public static void main(String[] args) throws IOException{ //punto de inicio de programa y arranca una exepcion en la que esta definida en el package java.io para qye exceptuelos errores de entra y salida
+        int A,B, Resultado = 0 ; //difinimos las variables A y B como integer y le damos el valor de 0.
+        char operador; //definimos operador como char.
+        boolean calculado = true; //difine calculodo como boolean con el valor true q es 0 true y 1 false. 
+        Scanner sc = new Scanner(System.in);//clase Scanner para recoger lo que introduzca del teclado.
+        System.out.print("Introduzca un numero entero:");//para mostrar lo que esta dentro del parentisis en pantalla.
+        A = sc.nextInt();//el valor introducido en pantalla lo guarada en A.
+        System.out.print("Introduzca otro numero entero:");//para mostrar lo que esta dentro del parentisis en pantalla.
+        B = sc.nextInt();//el valor introducido en pantalla lo guarada en B.
+        System.out.print("Introduzca un operador (+,-,*,/):");//para mostrar lo que esta dentro del parentisis en pantalla.
+        operador = (char)System.in.read();//guardamos el operador ingresa en la variable operador, El método read() acepta un byte proveniente del teclado y retorna un entero,Char convierte el entero  obtenido de read() en un caracter.
+        switch (operador) {//switch es una instruccion  para comparar una lista multiple posibilidades solo se utiliza con int o char.
+                case '-' : Resultado = A - B; //si el operador es -  nos hara la operacion A - B y la guardara en Resultado.
+                           break; //corta la ejecucion y no evalua las siguientes.
+                case '+' : Resultado = A + B; //si el operador es +  nos hara la operacion A + B y la guardara en Resultado.
+                           break; //corta la ejecucion y no evalua las siguientes.
+                case '*' : Resultado = A * B;  //si el operador es *  nos hara la operacion A * B y la guardara en Resultado.
+                           break; //corta la ejecucion y no evalua las siguientes.
+                case '/' : if(B!=0)  //si el operador es / y si la la variable B es diferente de 0  entonces entra al if.
+                              Resultado = A / B; //hara la division A / B y la guarda en Resultado
+                           else{ // si no cumplio las condiciones del if entra al else.
+                              System.out.println("\nNo se puede dividir por cero");//para mostrar lo que esta dentro del parentisis en pantalla.
+                              calculado = false;// le damos false a la variable calculado
                            }
-                           break;
-                default : System.out.println("\nOperador no valido");
-                          calculado = false;
+                           break; //corta la ejecucion y no evalua las siguientes.
+                default : System.out.println("\nOperador no valido");//si no se ingresa uno de los operadores  (+,-,*,/) y mostrara lo que esta dentro del parentisis en pantalla.
+                          calculado = false; // le dara false a la variable calculado.
                          
         }
-        if(calculado){
-            System.out.println("\nEl resultado es: " + Resultado);
+        if(calculado){ //si la variable se mantien en true como la hemos declarado en el principio entrara al if
+            System.out.println("\nEl resultado es: " + Resultado);  //mostrara lo que esta dentro del parentisis en pantalla.
         }
     }
 }
